8ffc274d428ca0cc283c6ac8f0a4fe488c26bb2a
Mahban Jafari <47887796+mahbangj@users.noreply.github.com>
2022-08-11 11:55:25 +0430

    Create StepperMotor_ATmega16.c

---
+++ StepperMotor_ATmega16.c
@@ -0,0 +1,221 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+� Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 7/25/2022
+Author  : 
+Company : 
+Comments: 
+
+
+Chip type               : ATmega16
+Program type            : Application
+AVR Core Clock frequency: 8.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 256
+*******************************************************/
+
+#include <mega16.h>
+#include <delay.h>
+
+#define TRIGGER   PORTA.0
+#define ECHO      PIND.6
+
+//#define steppin   PIND.3
+//#define dirpin    PIND.5
+//#define enable    PIND.4
+
+long timer = 0, xTimer = 0;
+float pulsa, distance;
+int i;
+int state;
+int direction;
+int setdir;
+
+// Timer1 overflow interrupt service routine
+interrupt [TIM1_OVF] void timer1_ovf_isr(void)
+{
+// Place your code here
+   xTimer++;
+}
+
+// Declare your global variables here
+
+void revmotor(void)
+{   
+  setdir = !setdir;  
+}
+
+void ultra() {
+   TRIGGER = 1;         
+   delay_us(10);
+   TRIGGER = 0;
+   while(ECHO == 0);    
+   TCNT1 = 0;           
+   xTimer = 0;          
+   while(ECHO == 1);    
+   timer = TCNT1;      
+   pulsa = (float)xTimer * 65535 * 0.5 + (float)timer * 0.5;   
+   distance = pulsa / 29.034 / 2;   
+}
+
+void main(void)
+{
+// Declare your local variables here
+    state=0;
+    setdir=0;
+// Input/Output Ports initialization
+// Port A initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=Out 
+DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (0<<DDA1) | (1<<DDA0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=0 
+PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+
+// Port B initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (0<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+// Port C initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=Out 
+DDRC=(0<<DDC7) | (0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (1<<DDC0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=0 
+PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+// Port D initialization
+// Function: Bit7=In Bit6=In Bit5=Out Bit4=Out Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRD=(1<<DDD7) | (0<<DDD6) | (1<<DDD5) | (1<<DDD4) | (1<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+// State: Bit7=T Bit6=T Bit5=0 Bit4=0 Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: Timer 0 Stopped
+// Mode: Normal top=0xFF
+// OC0 output: Disconnected
+
+
+TCCR0=0x00;
+TCNT0=0x00;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: Timer1 Stopped
+// Mode: Normal top=0xFFFF
+// OC1A output: Disconnected
+// OC1B output: Disconnected
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=0x00;
+TCCR1B=0x02;
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer2 Stopped
+// Mode: Normal top=0xFF
+// OC2 output: Disconnected
+ASSR=0x00;
+TCCR2=0x00;
+TCNT2=0x00;
+OCR2=0x00;
+
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=0x04;
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+// INT2: Off
+MCUCR=0x00;
+
+
+// USART initialization
+// USART disabled
+UCSRA=0x00;
+UCSRB=0x08;
+UCSRC=0x86;
+UBRRH=0x00;
+UBRRL=0x67;
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// The Analog Comparator's positive input is
+// connected to the AIN0 pin
+// The Analog Comparator's negative input is
+// connected to the AIN1 pin
+ACSR=0x80;
+SFIOR=0x00;
+
+// ADC initialization
+// ADC disabled
+ADCSRA=0x00;
+
+// SPI initialization
+// SPI disabled
+SPCR=0x00;
+
+// TWI initialization
+// TWI disabled
+TWCR=0x00;
+
+#asm("sei")
+
+
+while (1)
+      {
+
+      ultra(); 
+        
+
+       if(distance<0.5) //1=13cm
+       {
+        state=1;
+        PORTC.0=1;
+       }
+       else
+       {
+       state=2;
+       PORTC.0=0;
+       }
+
+      if(state==1)   
+      {    
+        revmotor();
+        PORTD.4=0;
+        PORTD.5=setdir; 
+        for(i=0;i<4000;i++)
+        {  
+        PORTD.3=1;
+        delay_us(20);
+        PORTD.3=0;
+        delay_us(20);
+        }
+        state=2; 
+      }
+      else
+      { 
+         PORTD.4=1;
+      }  
+
+      }
+}
